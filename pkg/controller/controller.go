1e2d05f417ba950bcfca682e7521e9185c159fc7
Jon Kent <jonkent95@gmail.com>
2023-02-08 09:16:26 +0000

    error handling and tests

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"fmt"
 	"image"
-	"image/jpeg"
 	"io"
 	"os"
 	"sdxImage/pkg/model"
@@ -13,7 +12,10 @@ import (
 )
 
 func Run(submission *model.Submission) image.Image {
-	survey := schema.Read(submission.SchemaName)
+	survey, err := schema.Read(submission.SchemaName)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	survey = substitutions.Replace(survey, submission)
 	survey = model.Add(survey, submission)
 	//fmt.Println(survey)
@@ -34,23 +36,4 @@ func readFile(schemaName string) []byte {
 	}(jsonFile)
 	bytes, _ := io.ReadAll(jsonFile)
 	return bytes
-}
-
-func saveJPG(path string, im image.Image, quality int) error {
-	file, err := os.Create(path)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}(file)
-
-	var opt jpeg.Options
-	opt.Quality = quality
-
-	return jpeg.Encode(file, im, &opt)
 }
