841ba22ad4f3952aadc80aff693f3821951a7832
Jon Kent <jonkent95@gmail.com>
2023-09-06 17:58:15 +0100

    Add Stocks schemas

--- pkg/schema/read_test.go
+++ pkg/schema/read_test.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"fmt"
 	"sdxImage/pkg/test"
+	"sort"
 	"testing"
 )
 
@@ -49,4 +50,38 @@ func TestReadUkis(t *testing.T) {
 		t.Errorf("failed to read ukis_0002 with error: %q", err.Error())
 	}
 	fmt.Println(result)
+}
+
+func TestReadStocks(t *testing.T) {
+	test.SetCwdToRoot()
+	set := map[string]struct{}{}
+	for x := 1; x < 10; x++ {
+		name := fmt.Sprintf("stocks_000%v", x)
+		readStock(name, set)
+	}
+	more := []int{10, 11, 12, 13, 14, 33, 34, 51, 52, 57, 58, 61, 70}
+	for _, y := range more {
+		name := fmt.Sprintf("stocks_00%v", y)
+		readStock(name, set)
+	}
+	list := make([]string, len(set))
+	i := 0
+	for k := range set {
+		list[i] = k
+		i++
+	}
+	sort.Strings(list)
+	for _, z := range list {
+		fmt.Println("\"" + z + "\":\"#" + z + "\",")
+	}
+}
+
+func readStock(name string, set map[string]struct{}) {
+	result, err := Read(name)
+	if err != nil {
+		fmt.Printf("failed to read %v with error: %q", name, err.Error())
+	}
+	for _, a := range result.Answers.AnswerMap {
+		set[a.Qcode] = struct{}{}
+	}
 }
