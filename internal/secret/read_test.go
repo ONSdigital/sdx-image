a19884b47ab8cbc2e216a54771457ee2c01bf379
Angus <Angus.goody@ons.gov.uk>
2026-01-08 12:35:06 +0000

    test

--- internal/secret/read_test.go
+++ internal/secret/read_test.go
@@ -1,5 +1,16 @@
 package secret
 
 import (
+	"fmt"
 	"testing"
 )
+
+func TestGetSecret(t *testing.T) {
+
+	// Not a unit test
+	secret, err := getSecret("iap-secret")
+	if err != nil {
+		return
+	}
+	fmt.Println(secret)
+}
